300f3a50e18d508e616df31d51782c1c8d813f61
Karim Shaloh <shal0054@algonquinlive.com>
2021-04-26 19:27:26 -0300

    started follow up page

--- main.js
+++ main.js
@@ -11,5 +11,24 @@ function materializeInit() {
 }
 
 function addListeners() {
-  
+  let nextBtn = document.getElementById('nextBtn');
+
+  if (nextBtn) {
+    nextBtn.addEventListener('click', () => {
+      let presFor = document.getElementById('presented-for').value;
+
+      switch (presFor) {
+        case 'assessment':
+          // go to assessment page
+          location.href = './assessment.html';
+          break;
+        case 'follow-up':
+          // go to follow up page
+          location.href = './follow-up.html';
+          break;
+        default:
+        // display warning
+      }
+    });
+  }
 }
