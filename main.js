df4d076ce9d52f3ebb5c14999a66c9474fed4677
Karim Shaloh <shal0054@algonquinlive.com>
2021-04-27 18:03:33 -0300

    back button on assessment screen works

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ function materializeInit() {
 
 function addListeners() {
   let nextBtnGen = document.getElementById('nextBtn-gen');
+  let backBtnAsses = document.getElementById('backBtn-asses');
+
+  let allSections = document.querySelectorAll('section');
+  let generalSection = document.getElementById('general');
+  let assessmentSection = document.getElementById('assessment');
+  let followUpSection = document.getElementById('follow-up');
 
   if (nextBtnGen) {
     nextBtnGen.addEventListener('click', () => {
       let presentedFor = document.getElementById('presented-for').value;
-      let generalSection = document.getElementById('general');
-      let assessmentSection = document.getElementById('assessment');
-      let followUpSection = document.getElementById('follow-up');
-      let allSections = document.querySelectorAll('section');
-
       switch (presentedFor) {
         case 'assessment':
           // hide all sections
@@ -30,18 +31,30 @@ function addListeners() {
           // show assessment screen
           assessmentSection.classList.remove('hidden');
           break;
-          case 'follow-up':
-            // hide all sections
-            allSections.forEach((section) => {
-              section.classList.add('hidden');
-            });
-            // show follow up screen
-            followUpSection.classList.remove('hidden');
+        case 'follow-up':
+          // hide all sections
+          allSections.forEach((section) => {
+            section.classList.add('hidden');
+          });
+          // show follow up screen
+          followUpSection.classList.remove('hidden');
 
           break;
         default:
         // display warning
       }
+    });
+  }
+
+  if (backBtnAsses) {
+    backBtnAsses.addEventListener('click', () => {
+      // hide all sections
+      allSections.forEach((section) => {
+        console.log(section);
+        section.classList.add('hidden');
+      });
+
+      generalSection.classList.remove('hidden');
     });
   }
 }
